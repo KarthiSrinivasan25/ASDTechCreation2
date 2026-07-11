83bbcb1fde4d6a88978f263c9298617dc2b68f70
KarthiSrinivasan25 <karthikeyan25082000@gmail.com>
2026-07-11 13:57:55 +0530

    first commit

---
+++ public/admin/assets/js/logout.js
@@ -0,0 +1,28 @@
+async function logout() {
+
+    const token = localStorage.getItem("admin_token");
+
+    try {
+
+        await fetch("https://karthikeyanportfolio.helioho.st/api/logout.php", {
+
+            method: "POST",
+
+            headers: {
+                "Authorization": "Bearer " + token
+            }
+
+        });
+
+    } catch (e) {
+        console.log(e);
+    }
+
+    // Clear browser storage
+    localStorage.removeItem("admin_token");
+    localStorage.removeItem("admin_name");
+    localStorage.removeItem("admin_email");
+
+    // Redirect
+    window.location.href = "login.html";
+}
